d40c926186eea6052cd3a5555d641c0f0d7cdbf3
김현희 <tta_bi@naver.com>
2022-02-09 11:12:36 +0900

    원하는 항목만 내려주기

--- copang_app/serializers.py
+++ copang_app/serializers.py
@@ -6,5 +6,5 @@ from .models import Users
 class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model= Users
-        fields = '__all__'
+        fields = ( 'id','name','email','phone','is_admin', 'image_url', 'created_at' )  # 우리가 원하는 항목만 내려주기
         
